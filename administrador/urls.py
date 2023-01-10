5b181851375d9551a8a3801f105c65e5279f7e01
Jonathan Nicolas Maciel Costa <20211094040012@ifrn.local>
2023-01-10 15:06:47 -0300

    Cadastro de professores e estudantes

--- administrador/urls.py
+++ administrador/urls.py
@@ -5,14 +5,22 @@ from .views import  home_view,\
                     sign_up_teacher_view,\
                     sign_up_course_view,\
                     sign_up_subject_view,\
-                    register_coordinator_view
+                    register_coordinator_view,\
+                    teste_view,\
+                    SignUpView_Student,\
+                    SignUpView_Teacher
 
 urlpatterns = [
 
     path('',  home_view, name = 'home'),
+
+    path('adm/sigh_up_student/cadastro_Estudante/', SignUpView_Student.as_view(), name='cadastro_estudante'),
+    path('adm/sigh_up_student/cadastro_Professor/', SignUpView_Teacher.as_view(), name='cadastro_professor'),
+
     path('adm/sigh_up_student',  sign_up_student_view, name = 'sign_up_student'),
     path('adm/sigh_up_teacher',  sign_up_teacher_view, name = 'sign_up_teacher'),
     path('adm/sigh_up_course',  sign_up_course_view, name = 'sign_up_course'),
     path('adm/sigh_up_subject',  sign_up_subject_view, name = 'sign_up_subject'),
     path('adm/register_coordinator',  register_coordinator_view, name = 'register_coordinator'),
+    path('adm/teste',  teste_view, name = 'teste'),
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,17 +1,77 @@
+from email.headerregistry import Group
+from tokenize import group
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Disciplina, Curso 
+
+from django.views.generic.edit import CreateView
+#create
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from Users.models import CustomUser
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Group
+
+from Users import forms
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 # Create your views here.
 
+@login_required(login_url='/user/login/')
 def home_view (request):
-    total_usuarios =  User.objects.count()
+
+    teacher_group = Group.objects.get(name='Professor')
+    student_group = Group.objects.get(name='Aluno')
+
+    total_de_professores = len(User.objects.filter(groups=teacher_group))
+    total_de_alunos = len(User.objects.filter(groups=student_group))
+    
     context = {
-        "users": total_usuarios,
+        "teacher": total_de_professores,
+        "student": total_de_alunos
     }
+
 
     return render(request, 'home.html',  context)
 
+class SignUpView_Student(CreateView):
+    form_class = forms.CustomUserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'sign_up_student.html'
+    model = CustomUser
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        student_group = Group.objects.get(name='Aluno')
+        self.object.groups.add(student_group)
+
+        messages.success(self.request, "Usuário cadastrado com sucesso!")
+        return response
+
+class SignUpView_Teacher(CreateView):
+    form_class = forms.CustomUserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'sign_up_teacher.html'
+    model = CustomUser
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        teacher_group = Group.objects.get(name='Professor')
+        self.object.groups.add(teacher_group)
+
+        messages.success(self.request, "Usuário cadastrado com sucesso!")
+        return response
+
+
+
 def sign_up_student_view (request):
+
+    
 
     return render(request, 'sign_up_student.html',  {})
 
@@ -31,6 +91,18 @@ def sign_up_subject_view (request):
 def register_coordinator_view (request):
 
     return render(request, 'register.coordinator.html',  {})
+
+def teste_view (request):
+    disciplina = Disciplina.objects.all()
+    curso = Curso.objects.all()
+
+    context = {
+        'disciplina' : disciplina,
+        'curso' : curso,
+    }
+    return render(request, 'teste.html',  context)
+
+
 
 def cadastro(request):
     if request.method == 'POST':
